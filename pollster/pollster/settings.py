46f185193a9f1a0a5eb035a1a5c078c480986fd5
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-10-31 11:38:18 +0200

    update configuration

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pollster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pollster_DB',
+        'USER': 'lithaxanti',
+        'PASSWORD': 'Vuy@ni2011',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
