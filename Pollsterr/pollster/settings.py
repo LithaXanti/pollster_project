1cf5570ee70c65a3de1f98e8020fee4c22ed52d8
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-10-31 15:57:31 +0200

    added secret key to env file

--- Pollsterr/pollster/settings.py
+++ Pollsterr/pollster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!!jr^)=*j%r1(am=nz7*q9aehza-!thc29u^bd3l5kew853en'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
