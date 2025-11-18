0940ff3da3fb612a75d62cfc39fd46cb3e9b762a
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-11-18 15:08:31 +0200

    made more changes

--- Pollsterr/pollster/settings.py
+++ Pollsterr/pollster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.247.230.70']
+ALLOWED_HOSTS = ['13.247.60.41']
 
 
 # Application definition
