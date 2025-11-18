563c6fd6c9e1c01708c9d5c28322f6ed696397c0
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-11-18 15:13:46 +0200

    more changes

--- Pollsterr/pollster/settings.py
+++ Pollsterr/pollster/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'pollster.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'pollster_db'),
-        'USER': os.environ.get('DB_USER', 'lithaxanti'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Vuy@ni2011'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
