d3817c87410e01ea84d3a723b0e81b35f9913483
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-11-18 16:10:15 +0200

    edits

--- Pollsterr/pollster/settings.py
+++ Pollsterr/pollster/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['13.247.60.41']
+ALLOWED_HOSTS = ['127.0.0.1/8000/']
 
 
 # Application definition
